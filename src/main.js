d5607afed8bb865705864af401267cb07c0a9a77
CatherineChoque <85088235+CatherineChoque@users.noreply.github.com>
2021-06-29 17:42:08 -0500

    Nav and footer

--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+addEventListener('DOMContentLoaded', () => {
+    const buttonMenu = document.querySelector('.buttonMenu')
+    if(buttonMenu) {
+        buttonMenu.addEventListener('click',() => {
+            const menuItems = document.querySelector('.menuItems')
+            menuItems.classList.toggle('show');
+        })
+    }
+})
 
-console.log(example, data);
+
+
+//console.log(example, data);
