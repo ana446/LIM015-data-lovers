a9d369179403227d92f0cf32aa23d6791da964af
CatherineChoque <85088235+CatherineChoque@users.noreply.github.com>
2021-07-19 08:56:22 -0500

    Table stats

--- src/main.js
+++ src/main.js
@@ -202,33 +202,7 @@ allChampion.addEventListener('click', (e) => {
     // sectionElementRol.innerHTML = labelOrder;
     // sectionElementRol.appendChild(selectOrderAlphabet);
     showChampions(dataLolArray);
-
-    
-        /********** texto de filtrado ***********/
-        const mayormenor = 
-        ` 
-            <h2 id="mayor">Ascendente</h2> 
-            <h2 id="menor">Descendente</h2> 
-        `; 
-        sectionElementRol.innerHTML = mayormenor;
-    
-        /****** ascendente *******/
-        const mayorAs = document.querySelector("#mayor");
-        mayorAs.addEventListener("click", () => {
-            e.preventDefault();
-
-            dataLolArray.forEach ( (value) => {
-                const soloDifficulty = value.info.difficulty;
-                const numeros = [soloDifficulty];
-                numeros.sort((a,b) => b.numeros - a.numeros);
-                console.log(numeros);
-            })
-        }
-        );
-        /****** descendente *******/
-
-        /****** *******/
-});
+})
 
 //filterByRole
 const assassinRols = document.querySelector('.assassinRols');
@@ -392,22 +366,17 @@ lowDifficulty.addEventListener("click",(e)=>{
     e.preventDefault();
     const rangeDifficulty = 3;
     mainCardsLol.innerHTML ="";
-
     const getLowDifficulty =  dataLolArray.filter((e)=>( e.info.difficulty <= rangeDifficulty));
-
     bannerLol.style.display ="none";
     mainSliderLol.style.display ="none";
     orderChampionsLol.style.display="none";
     sectionElementRol.innerHTML="";
     
     const showLowDifficulty = showFilterByDifficulty(getLowDifficulty);
-
     showChampions(showLowDifficulty);
     
 
 });
-
-
 moderateDifficulty.addEventListener("click",(e)=>{
     e.preventDefault();
     const rangeDifficulty = 3;
@@ -437,7 +406,7 @@ highDifficulty.addEventListener("click",()=>{
 
 // order AZ ZA
 const alphabeticalOrder = document.querySelector('#alphabeticalOrder');
-console.log(alphabeticalOrder);
+//console.log(alphabeticalOrder);
 alphabeticalOrder.addEventListener('change', (e)=>{
     e.preventDefault();
     const optionSelect = e.target.value;
@@ -498,7 +467,6 @@ document.body.addEventListener('click', (e) =>{
                     <div class="boxChampion">
                         <img src="${findChampion.splash}">
                     </div>
-
                     <div class="blurbInformation">
                         <div class ="titleChampion">
                             <p>${findChampion.title}</p>
@@ -522,6 +490,41 @@ document.body.addEventListener('click', (e) =>{
         closeModal.addEventListener('click',function closeModal(){
             divMyChampionModal.remove('myChampionModal');
         });
-
-        }
+    }
 })
+
+// champions stat
+const championsStat = document.querySelector("#championsStat");
+
+championsStat.addEventListener( 'click', (e) => {
+    e.preventDefault();
+    sectionElementRol.remove();
+    orderChampionsLol.remove();
+    bannerLol.remove();
+    mainSliderLol.remove();
+    mainCardsLol.remove();
+
+    const tableStats = document.querySelector('#tableStats');
+    tableStats.style.display = "block";
+    tableStats.style.display = "flex";
+    tableStats.style.justifyContent = "center";
+    const tableBody = document.querySelector('#tablebody');
+    //mainContainer.innerHTML = "";
+
+    dataLolArray.forEach( (data) =>{
+
+            let nameChampions = ` <td>${data.name}</td> `;
+            let imgChampions = `<td><img src="${data.img}"></td> `
+            let hpChampions = ` <td>${data.stats.hp}</td> `;
+            let mpChampions = ` <td>${data.stats.mp}</td> `;
+            let armorChampions = ` <td>${data.stats.armor}</td> `;
+            let attackrChampions = ` <td>${data.stats.attackrange}</td> `;
+            let moveChampions = ` <td>${data.stats.movespeed}</td> `;
+            let attackdChampions = ` <td>${data.stats.attackdamage}</td> `;
+
+            const allTable = `<tr>${imgChampions+nameChampions+hpChampions+mpChampions+armorChampions+attackrChampions+moveChampions+attackdChampions}</tr>`
+
+            tableBody.innerHTML += allTable;
+            console.log(allTable);
+        })
+} )
